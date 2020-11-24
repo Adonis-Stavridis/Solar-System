3d2911073cd9a654fa410176a918062b74a098dd
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-11-24 18:20:46 +0100

    working + real distance and scale values

--- projet.js
+++ projet.js
@@ -1,34 +1,134 @@
 "use strict"
 
-//--------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // GLSL
-//--------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 // All the vertex and fragment shaders ...
+var vertexShader = `#version 300 es
+layout(location=0) in vec3 position_in;
+layout(location=1) in vec3 normals_in;
+layout(location=2) in vec2 texture_in;
 
-//--------------------------------------------------------------------------------------------------------
+uniform mat4 uProjMat;
+uniform mat4 uViewMat;
+uniform mat4 uModeMat;
 
-// Global variables : textures, FBOs, prog_shaders, mesh, renderer, and a lot of parameters
+out vec2 texCoord;
+
+void main()
+{
+  texCoord = texture_in;
+  gl_Position = uProjMat * uViewMat * uModeMat * vec4( position_in, 1.0 );
+}
+`;
+
+var fragmentShader = `#version 300 es
+precision highp float;
+
+in vec2 texCoord;
+
+uniform sampler2D uTexture;
+
+out vec4 oFragmentColor;
+
+void main()
+{
+  oFragmentColor = texture(uTexture, texCoord);
+}
+`;
+
+//------------------------------------------------------------------------------
+
+// Global variables : textures, FBOs, prog_shaders, mesh, renderer, and a lot of
+// parameters
+
+let textures = {};
+
+let mesh = null;
+let meshRenderer = null;
+
+let shaderProgram = null;
+
+let planets = {
+  'sun': {
+    'distance': 0,
+    'scale': 695700 ,
+    'rotation': 0
+  },
+  'mercury': {
+    'distance': 57.91,
+    'scale': 2439,
+    'rotation': 0
+  },
+  'venus': {
+    'distance': 108.2,
+    'scale': 6051,
+    'rotation': 0
+  },
+  'earth':  {
+    'distance': 149.6,
+    'scale': 6371,
+    'rotation': 0
+  },
+  'mars':  {
+    'distance': 227.9,
+    'scale': 3389,
+    'rotation': 0
+  },
+  'jupiter':  {
+    'distance': 778.5,
+    'scale': 69911,
+    'rotation': 0
+  },
+  'uranus':  {
+    'distance': 1434,
+    'scale': 25362,
+    'rotation': 0
+  },
+  'neptune':  {
+    'distance': 2871,
+    'scale': 24622,
+    'rotation': 0
+  }
+};
 
 function init_wgl() {
   ewgl.continuous_update = true;
 
   // Generate all the textures
-  let tex = Texture2d();
-  tex.load("path", gl.RGB8);
-  // ...
+  for (var value in planets) {
+    let tex = Texture2d(
+      [gl.TEXTURE_MAG_FILTER, gl.LINEAR],
+      [gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR],
+      [gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE],
+      [gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE],
+      [gl.TEXTURE_BASE_LEVEL, 0],
+      [gl.TEXTURE_COMPARE_FUNC, gl.LEQUAL],
+      [gl.TEXTURE_COMPARE_MODE, gl.COMPARE_REF_TO_TEXTURE],
+      [gl.TEXTURE_MAX_LEVEL, 5],
+      [gl.TEXTURE_MAX_LOD, 0.0],
+      [gl.TEXTURE_MIN_LOD, 5.0],
+      [gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE]
+    );
+    tex.load('images/' + value + '.jpg', gl.RGB8);
+    textures[value] = tex;
+  }
 
   // texture cubeMap for the skybox
-  let tex_skybox = TextureCubeMap();
-  tex_skybox.load(["...", "...", "...", "...", "...", "..."]).then(update_wgl);
+  // let tex_skybox = TextureCubeMap();
+  // tex_skybox.load(["...", "...", "...", "...", "...", "..."]).then(update_wgl);
+
   // Create a mesh cube and his renderer
   // ...
 
   // Create all the shader programs
+  shaderProgram = ShaderProgram(vertexShader, fragmentShader, 'basicShader');
   // ...
 
   // Create a mesh of a sphere and a renderer
-  let mesh;
+  mesh = Mesh.Sphere(32);
+  meshRenderer = mesh.renderer(0, 1, 2);
   // ...
 
   // Set the radius and the center of the scene
@@ -36,73 +136,90 @@ function init_wgl() {
   ewgl.scene_camera.set_scene_center(mesh.BB.center);
 
   // Asteroid Belt
-  // ----------------------------------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
 
-  // Shader Program for asteroÃ¯ds
+  // Shader Program for asteroids
   // ...
 
-  // Create a typed array to contain all the 4x4 model matrices of each asteroÃ¯d
-  const matrixData = new Float32Array(4 * 4 * nbAsteroids);
-  // For each asteroÃ¯d
-  for (let i = 0; i < nbAsteroids; ++i) {
-    var model;
+  // Create a typed array to contain all the 4x4 model matrices of each asteroid
+  //   let nbAsteroids = 0;
 
-    // Compute a matrix model
+  //   const matrixData = new Float32Array(4 * 4 * nbAsteroids);
+  //   // For each asteroid
+  //   for (let i = 0; i < nbAsteroids; ++i) {
+  //     var model;
 
-    // Put the matrix model a the right place in the typed array
-    var index = 16 * i;
-    matrixData.set(model.data, index);
-  }
+  //     // Compute a matrix model
 
-  // VBO for model matrix of each instance
-  const matrixBuffer = VBO(matrixData);
+  //     // Put the matrix model a the right place in the typed array
+  //     var index = 16 * i;
+  //     matrixData.set(model.data, index);
+  //   }
 
-  // Load the .obj mesh and use an instanced renderer (with 4 VBO, to recreate a 4x4 matrix) to get a lot of asteroÃ¯ds
-  Mesh.loadObjFile("rock/rock.obj").then((meshes) => {
-    rock_rend = meshes[0].instanced_renderer([
-      [3, matrixBuffer, 1, 4 * 4, 0 * 4, 4],
-      [4, matrixBuffer, 1, 4 * 4, 1 * 4, 4],
-      [5, matrixBuffer, 1, 4 * 4, 2 * 4, 4],
-      [6, matrixBuffer, 1, 4 * 4, 3 * 4, 4]],
-      0, 1, 2);
-  });
-  // then, the matrice of an instance can be retrieved in a vertex shader with : layout(location=3) in mat4 instanceMatrix;
-  // ----------------------------------------------------------------------------------------------------
+  //   // VBO for model matrix of each instance
+  //   const matrixBuffer = VBO(matrixData);
 
-  // ATMOSPHERE (GLOW)
-  // Create Shader programs ...
-  // Create FBOs with the linked textures ...
+  //   // Load the .obj mesh and use an instanced renderer (with 4 VBO, to recreate a 4x4 matrix) to get a lot of asteroids
+  //   Mesh.loadObjFile("rock/rock.obj").then((meshes) => {
+  //     rock_rend = meshes[0].instanced_renderer([
+  //       [3, matrixBuffer, 1, 4 * 4, 0 * 4, 4],
+  //       [4, matrixBuffer, 1, 4 * 4, 1 * 4, 4],
+  //       [5, matrixBuffer, 1, 4 * 4, 2 * 4, 4],
+  //       [6, matrixBuffer, 1, 4 * 4, 3 * 4, 4]],
+  //       0, 1, 2);
+  //   });
+  //   // then, the matrice of an instance can be retrieved in a vertex shader with : layout(location=3) in mat4 instanceMatrix;
+  //   // ----------------------------------------------------------------------------------------------------
 
-  // User interface
-  UserInterface.begin();
-  // ...
-  UserInterface.end();
+  //   // ATMOSPHERE (GLOW)
+  //   // Create Shader programs ...
+  //   // Create FBOs with the linked textures ...
+
+  //   // User interface
+  //   UserInterface.begin();
+  //   // ...
+  //   UserInterface.end();
 }
 
-function getRandomMax(max) {
-  return Math.random() * Math.floor(max);
-}
+// function getRandomMax(max) {
+//   return Math.random() * Math.floor(max);
+// }
 
-function getRandomMinMax(min, max) {
-  return Math.random() * (max - min) + min;
-}
+// function getRandomMinMax(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
 
-function resize_wgl(w, h) {
-  let d = Math.pow(2, 3);
-  // 
-  fbo1.resize(w / d, h / d);
-  fbo2.resize(w / d, h / d);
-  // Faire varier l'intensiter selon la taille
-  // glow_intensity = 300 - ((w/100) * (h/100));
-}
+// function resize_wgl(w, h) {
+//   let d = Math.pow(2, 3);
+//   // 
+//   fbo1.resize(w / d, h / d);
+//   fbo2.resize(w / d, h / d);
+//   // Faire varier l'intensite selon la taille
+//   // glow_intensity = 300 - ((w/100) * (h/100));
+// }
 
-// -------------------------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 //  DRAW
-// -------------------------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 function draw_wgl() {
   gl.clearColor(0, 0, 0, 0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.enable(gl.DEPTH_TEST);
+
+  shaderProgram.bind();
+
+  Uniforms.uProjMat = ewgl.scene_camera.get_projection_matrix();
+  Uniforms.uViewMat = ewgl.scene_camera.get_view_matrix();
+  for (var value in planets) {
+    let modelMatrix = Matrix.mult(
+      getDistance(planets[value]),
+      Matrix.rotateX(90),
+      getScale(planets[value])
+    );
+    Uniforms.uModeMat = modelMatrix;
+    Uniforms.uTexture = textures[value].bind(0);
+    meshRenderer.draw(gl.TRIANGLES);
+  }
 
   // set scene center according to the selected object
   // ...
@@ -126,6 +243,14 @@ function draw_wgl() {
   // Render asteroids
   // ...
 
+}
+
+function getDistance(planet) {
+  return Matrix.translate(planet['distance'] / planets['sun']['scale'], 0, 0);
+}
+
+function getScale(planet) {
+  return Matrix.scale(planet['scale'] / planets['sun']['scale']);
 }
 
 function mousedown_wgl(ev) {
