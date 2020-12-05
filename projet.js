d755ef5e6042cb18b6612b4d4e04dc33a184003e
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-12-05 17:46:40 +0100

    earth shader

--- projet.js
+++ projet.js
@@ -33,7 +33,7 @@ void main()
 }
 `;
 
-let sunVertexShader = `#version 300 es
+let basicVertexShader = `#version 300 es
 layout(location=0) in vec3 position_in;
 layout(location=1) in vec3 normal_in;
 layout(location=2) in vec2 texture_in;
@@ -63,24 +63,6 @@ out vec4 oFragmentColor;
 void main()
 {
   oFragmentColor = texture(uTexture, texCoord_out);
-}
-`;
-
-let pathVertexShader = `#version 300 es
-layout(location=0) in vec3 position_in;
-layout(location=1) in vec3 normal_in;
-layout(location=2) in vec2 texture_in;
-
-uniform mat4 uProjMat;
-uniform mat4 uViewMat;
-uniform mat4 uModeMat;
-
-out vec2 texCoord_out;
-
-void main()
-{
-  texCoord_out = texture_in;
-  gl_Position = uProjMat * uViewMat * uModeMat * vec4( position_in, 1.0 );
 }
 `;
 
@@ -143,10 +125,60 @@ void main()
   vec3 Ia = uLightIntensity * vec3(0.0, 0.0, 0.0);
 
   vec4 tex = texture(uTexture, texCoord_out);
+
   vec3 lightDirection = normalize(uLightPosition - position_out);
   float diffuseElement = max(0.0, dot(normal_out, lightDirection));
 
   vec3 Id = uLightIntensity * tex.rgb * vec3(diffuseElement);
+  Id = Id / M_PI;
+
+  float uNs = 128.0;
+  vec3 viewDirection = normalize(-position_out.xyz);
+  vec3 halfDirection = normalize(viewDirection + lightDirection);
+  float specularElement = pow(dot(normal_out, halfDirection), uNs);
+
+  vec3 Is = uLightIntensity * vec3(1.0, 1.0, 1.0) * vec3(specularElement);
+  Is /= (uNs + 2.0) / (2.0 * M_PI);
+
+  vec3 color = (0.3 * Ia) + (0.3 * Id) + (0.3 * Is);
+
+  oFragmentColor = vec4(color, 1.0);
+}
+`;
+
+let earthFragmentShader = `#version 300 es
+precision highp float;
+
+#define M_PI 3.14159265358979
+
+in vec2 texCoord_out;
+in vec3 position_out;
+in vec3 normal_out;
+
+uniform sampler2D uTexture;
+uniform sampler2D uTextureClouds;
+uniform sampler2D uTextureNight;
+uniform float uLightIntensity;
+uniform vec3 uLightPosition;
+
+out vec4 oFragmentColor;
+
+void main()
+{
+  vec3 Ia = uLightIntensity * vec3(0.0, 0.0, 0.0);
+
+  vec4 tex = texture(uTexture, texCoord_out);
+  vec4 texClouds = texture(uTextureClouds, texCoord_out);
+  vec4 texNight = texture(uTextureNight, texCoord_out);
+
+  vec3 lightDirection = normalize(uLightPosition - position_out);
+  float diffuseElement = max(0.0, dot(normal_out, lightDirection));
+  
+  vec3 IdDay = uLightIntensity * tex.rgb * vec3(diffuseElement);
+  vec3 IdClouds = uLightIntensity * texClouds.rgb  * vec3(diffuseElement);
+  vec3 IdTemp = IdDay + IdClouds;
+  vec3 IdNight = uLightIntensity * texNight.rgb * vec3(max(0.0, 1.0 - (2.0 * diffuseElement)));
+  vec3 Id = mix(IdTemp, IdNight, 0.5);
   Id = Id / M_PI;
 
   float uNs = 128.0;
@@ -398,6 +430,76 @@ class Planet extends Body {
   }
 }
 
+class Earth extends Planet {
+  constructor(name, distanceToSun, scale, incline, yearPeriod, dayPeriod, shader, meshRenderer, pathShader, sun) {
+    super(name, distanceToSun, scale, incline, yearPeriod, dayPeriod, shader, meshRenderer, pathShader, sun);
+    
+    let texClouds = Texture2d(
+      [gl.TEXTURE_MAG_FILTER, gl.LINEAR],
+      [gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE],
+      [gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE],
+      [gl.TEXTURE_BASE_LEVEL, 0],
+      [gl.TEXTURE_COMPARE_FUNC, gl.LEQUAL],
+      [gl.TEXTURE_COMPARE_MODE, gl.COMPARE_REF_TO_TEXTURE],
+      [gl.TEXTURE_MAX_LEVEL, 5],
+      [gl.TEXTURE_MAX_LOD, 0.0],
+      [gl.TEXTURE_MIN_LOD, 5.0],
+      [gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE]
+    );
+    texClouds.load('images/' + name + '_clouds.jpg', gl.RGB8);
+    this.textureClouds = texClouds;
+
+    let texNight = Texture2d(
+      [gl.TEXTURE_MAG_FILTER, gl.LINEAR],
+      [gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE],
+      [gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE],
+      [gl.TEXTURE_BASE_LEVEL, 0],
+      [gl.TEXTURE_COMPARE_FUNC, gl.LEQUAL],
+      [gl.TEXTURE_COMPARE_MODE, gl.COMPARE_REF_TO_TEXTURE],
+      [gl.TEXTURE_MAX_LEVEL, 5],
+      [gl.TEXTURE_MAX_LOD, 0.0],
+      [gl.TEXTURE_MIN_LOD, 5.0],
+      [gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE]
+    );
+    texNight.load('images/' + name + '_night.jpg', gl.RGB8);
+    this.textureNight = texNight;
+  }
+
+  render() {
+    this.shader.bind();
+
+    Uniforms.uProjMat = ewgl.scene_camera.get_projection_matrix();
+    Uniforms.uViewMat = ewgl.scene_camera.get_view_matrix();
+
+    let inclineBodyMat = this.inclineBody();
+    let dayPeriodBodyMat = this.dayPeriodBody();
+    let alignBodyMat = this.alignBody();
+    let scaleBodyMat = this.scaleBody();
+
+    this.anchor = this.getAnchor;
+
+    let modelMatrix = Matrix.mult(
+      this.anchor,
+      inclineBodyMat,
+      dayPeriodBodyMat,
+      alignBodyMat,
+      scaleBodyMat
+    );
+    Uniforms.uModeMat = modelMatrix;
+    let mvm = Matrix.mult(ewgl.scene_camera.get_view_matrix(), modelMatrix);
+    Uniforms.uNormMat = mvm.inverse3transpose();
+
+    Uniforms.uTexture = this.texture.bind(0);
+    Uniforms.uTextureClouds = this.textureClouds.bind(1);
+    Uniforms.uTextureNight = this.textureNight.bind(2);
+    Uniforms.uLightPosition = sun.getLightPosition;
+    Uniforms.uLightIntensity = sun.getLightIntensity;
+    this.meshRenderer.draw(gl.TRIANGLES);
+
+    gl.useProgram(null);
+  }
+}
+
 //------------------------------------------------------------------------------
 
 class Interface {
@@ -456,9 +558,10 @@ function init_wgl() {
     'images/skybox/skybox_milky_way.png'
   ], skyboxShader, skyboxRenderer);
 
-  let pathShader = ShaderProgram(pathVertexShader, pathFragmentShader, 'pathShader');
+  let pathShader = ShaderProgram(basicVertexShader, pathFragmentShader, 'pathShader');
   let planetShader = ShaderProgram(planetVertexShader, planetFragmentShader, 'planetShader');
-  let sunShader = ShaderProgram(sunVertexShader, sunFragmentShader, 'sunShader');
+  let earthShader = ShaderProgram(planetVertexShader, earthFragmentShader, 'earthShader');
+  let sunShader = ShaderProgram(basicVertexShader, sunFragmentShader, 'sunShader');
   let mesh = Mesh.Sphere(64);
   let meshRenderer = mesh.renderer(0, 1, 2);
 
@@ -468,7 +571,7 @@ function init_wgl() {
     'sun': sun,
     'mercury': new Planet('mercury', 2.2, 0.02, 0.1, 88.0, 58.64 * EARTH_DAY__PERIOD, planetShader, meshRenderer, pathShader, sun),
     'venus': new Planet('venus', 3, 0.05, 177, 224.7, -243.01 * EARTH_DAY__PERIOD, planetShader, meshRenderer, pathShader, sun),
-    'earth': new Planet('earth', 4.5, 0.1, 24, EARTH_YEAR_PERIOD, EARTH_DAY__PERIOD, planetShader, meshRenderer, pathShader, sun),
+    'earth': new Earth('earth', 4.5, 0.1, 24, EARTH_YEAR_PERIOD, EARTH_DAY__PERIOD, earthShader, meshRenderer, pathShader, sun),
     'mars': new Planet('mars', 6, 0.08, 25, 689.0, 24.62, planetShader, meshRenderer, pathShader, sun),
     'jupiter': new Planet('jupiter', 15, 0.4, 3, 11.87 * EARTH_YEAR_PERIOD, 9.92, planetShader, meshRenderer, pathShader, sun),
     'saturn': new Planet('saturn', 22, 0.3, 27, 29.45 * EARTH_YEAR_PERIOD, 10.65, planetShader, meshRenderer, pathShader, sun),
